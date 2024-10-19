e84321adb91b1d4b7983a8fe0876793d30defc12
pathan17 <badhanpathan17@gmail.com>
2024-10-19 17:45:49 +0600

    shopMenuADDED

--- src/Components/ShopMenWomenKids.jsx
+++ src/Components/ShopMenWomenKids.jsx
@@ -8,9 +8,8 @@ const ShopMenWomenKids = () => {
                 <div className="container mx-auto">
                     <div className="Shop flex flex-col sm:flex-col lg:flex-row gap-4 lg:gap-10">
                         <div className="shopNow w-full lg:w-2/3 sm:w-full sm:mx-auto">
-                            <div className="flex justify-between items-center sm:mt-[20px]">
-                                <h1 className="mt-4">Shop Now</h1>
-                                
+                            <div className="flex justify-between items-center sm:mt-[20px] lg:mt-[-6px]">
+                                <h1 className="">Shop Now</h1>
                                     <h1 className="mt-4">MEN</h1>
                                     <h1 className="mt-4">WOMEN</h1>
                                     <h1 className="mt-4">KIDS</h1>
@@ -18,18 +17,18 @@ const ShopMenWomenKids = () => {
                             </div>
                         </div>
                         <div className="shopNow w-full lg:w-1/3 sm:mx-auto bg-slate-100 lg:bg-inherit lg:mt-0 md:w-full">
-                            <div className="Icon flex flex-col sm:flex-row gap-4 sm:gap-1 p-4 justify-center items-center">
+                            <div className="Icon flex flex-col sm:flex-row gap-4 sm:gap-1  justify-center items-center">
                                 <div className="text">
-                                    <h1 className="mt-4">GET 5% OFF ON APP</h1>
+                                    <h1 className="">GET 5% OFF ON APP</h1>
                                 </div>
-                                <div className="text flex justify-center items-center px-4 bg-black rounded-md mt-3">
+                                <div className="text flex justify-center items-center px-4 bg-black rounded-md  ">
                                     <FaGooglePlay className="text-red-400" />
                                     <span className="inline-block ml-1 text-white">
                                         <p className="text-[10px]">GET IT ON</p> Google Play
                                     </span>
                                 </div>
                                 <div className="text">
-                                    <div className="flex justify-center items-center px-4 bg-black rounded-md mt-3">
+                                    <div className="flex justify-center items-center px-4 bg-black rounded-md ">
                                         <FaApple className="text-white" />
                                         <span className="inline-block text-white">
                                             <p className="text-[10px]">GET IT ON</p> App Store
