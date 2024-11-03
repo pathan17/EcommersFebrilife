83cadc6826ec3112f812c207f84c4a53cd7611f9
pathan17 <badhanpathan17@gmail.com>
2024-11-03 17:18:55 +0600

    shopMenWomenKids uppdate

--- src/Components/HomeComponent/ShopMenWomenKids.jsx
+++ src/Components/HomeComponent/ShopMenWomenKids.jsx
@@ -7,7 +7,7 @@ const ShopMenWomenKids = () => {
             <div className="main-shop   sm:mt-2 bg-slate-100 h-[75px] lg:h-[60px] md:h-[100px] sm:mb-[100px] lg:mb-5 lg:mt-[-28px]">
                 <div className="container mx-auto">
                     <div className="Shop flex flex-col sm:flex-col lg:flex-row gap-4 lg:gap-10 ">
-                        <div className="shopNow w-full lg:w-2/3 sm:w-full sm:mx-auto ">
+                        <div className="shopNow w-full lg:w-[50%] sm:w-full sm:mx-auto ">
                             <div className="flex justify-between items-center sm:mt-[20px]">
                                     <h1 className=" ">Shop Now</h1>
                                     <h1 className="">MEN</h1>
@@ -16,8 +16,8 @@ const ShopMenWomenKids = () => {
                               
                             </div>
                         </div>
-                        <div className="shopNow w-full lg:w-1/3 sm:mx-auto bg-slate-100 p-3 lg:p-0 lg:bg-inherit lg:mt-[12px] md:w-full">
-                            <div className="Icon flex flex-col sm:flex-row gap-2 sm:gap-1  justify-center items-center">
+                        <div className="shopNow w-full lg:w-[50%] sm:mx-auto bg-slate-100 p-3 lg:p-0 lg:bg-inherit lg:mt-[12px] md:w-full">
+                            <div className="Icon flex flex-col sm:flex-row gap-2 sm:gap-1 lg:gap-5  justify-center items-center">
                                 <div className="text">
                                     <h1 className="">GET 5% OFF ON APP</h1>
                                 </div>
@@ -30,7 +30,7 @@ const ShopMenWomenKids = () => {
                                 <div className="text">
                                     <div className="flex justify-center items-center px-4 bg-black rounded-md ">
                                         <FaApple className="text-white" />
-                                        <span className="inline-block text-white">
+                                        <span className="inline-block text-white lg:ml-4">
                                             <p className="text-[10px]">GET IT ON</p> App Store
                                         </span>
                                     </div>
