b8027050cb86273c2e2f7c11810ff1fc2feceb80
pathan17 <badhanpathan17@gmail.com>
2024-11-05 15:04:25 +0600

    shopMenWomen added

--- src/Components/HomeComponent/ShopMenWomenKids.jsx
+++ src/Components/HomeComponent/ShopMenWomenKids.jsx
@@ -4,10 +4,10 @@ import { FaApple } from "react-icons/fa";
 const ShopMenWomenKids = () => {
     return (
         <div>
-            <div className="main-shop md:px-[40px] sm:mt-2 bg-slate-100 h-[75px] lg:h-[60px] md:h-[100px] sm:mb-[100px] lg:mb-5 lg:mt-[-28px]">
+            <div className="main-shop md:px-[40px] px-4 sm:mt-2 bg-slate-100 h-[75px] lg:h-[60px] md:h-[100px] sm:mb-[100px] lg:mb-5 lg:mt-[-28px]">
                 <div className="container mx-auto">
-                    <div className="Shop flex flex-col sm:flex-col lg:flex-row gap-4 lg:gap-[50px] ">
-                        <div className="shopNow w-full lg:w-[50%] sm:w-full md:w-full sm:mx-auto ">
+                    <div className="Shop flex flex-col sm:flex-col lg:flex-row gap-3 lg:gap-[60px]">
+                        <div className="shopNow w-full lg:w-[50%] sm:w-full md:w-[30%] sm:mx-auto ">
                             <div className="flex justify-between items-center sm:mt-[20px] uppercase font-sans">
                                     <h1 className=" ">Shop Now</h1>
                                     <h1 className="">MEN</h1>
@@ -16,22 +16,22 @@ const ShopMenWomenKids = () => {
                               
                             </div>
                         </div>
-                        <div className="shopNow w-full lg:w-[50%] sm:mx-auto bg-slate-100 p-3 md:p-0 lg:p-0 lg:bg-inherit lg:mt-[12px] md:w-full hover:text-red-600">
-                            <div className="Icon flex flex-col sm:flex-row gap-2 sm:gap-1 lg:gap-5  justify-center items-center">
+                        <div className="shopNow w-full lg:w-[30%] md:w-[70%] sm:mx-auto bg-slate-100   lg:p-0 lg:bg-inherit hover:text-red-600 ">
+                            <div className="Icon flex flex-col sm:flex-row gap-2 md:gap-0  sm:gap-1 lg:gap-5   justify-center items-center  lg:mt-[12px]  md:mt-[-10px]">
                                 <div className="text">
-                                    <h1 className=" font-sans ">GET 5% OFF ON APP</h1>
+                                    <h1 className=" font-sans capitalize">GET 5% OFF ON APP</h1>
                                 </div>
-                                <div className="text flex justify-center items-center px-4 md:px-2 bg-black rounded-md  ">
+                                <div className="text flex justify-center items-center px-4 md:px-2 lg:px-3 lg:py-2 md:gap-[5px]  bg-black rounded-md  ">
                                     <FaGooglePlay className="text-red-400" />
-                                    <span className="inline-block ml-1 text-white">
+                                    <span className=" ml-1 text-white md:text-[5px]">
                                         <p className="text-[10px]">GET IT ON</p> Google Play
                                     </span>
                                 </div>
                                 <div className="text">
-                                    <div className="flex justify-center items-center px-4 md:px-2 bg-black rounded-md  ">
-                                        <FaApple className="text-white" />
-                                        <span className="inline-block text-white lg:ml-4">
-                                            <p className="text-[10px]">GET IT ON</p> App Store
+                                    <div className="flex justify-center items-center px-4 md:px-2 lg:px-3 lg:py-2  bg-black rounded-md  ">
+                                        <FaApple className="text-white " />
+                                        <span className="inline-block text-white lg:ml-2 md:text-[5px]">
+                                            <p className="text-[10px]  ">GET IT ON</p> App Store
                                         </span>
                                     </div>
                                 </div>
